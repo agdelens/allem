788977282f91295ebd5a387588eb99181f22a3fa
Serkan Agdelen <agdelens@gmail.com>
2020-04-08 23:00:19 +0300

    commit

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const { createHttpTerminator } = require('http-terminator');
 const app = new Koa();
 app.use(bodyParser());
 
+// Settings
 const HOST = process.env.HOST || '127.0.0.1';
 const PORT = process.env.PORT || 5000;
 
@@ -14,25 +15,33 @@ app.use(async ctx => {
   console.log(JSON.stringify(ctx.request.body))
   ctx.body = { msg: 'Hello World' };
 });
-
+// Listen
 const server = http
   .createServer(app.callback())
   .listen(PORT, HOST, listeningReporter)
 
 const dalek = createHttpTerminator({ server });
 
+// A function that runs in the context of the http server
+// and reports what type of server listens on which port
 function listeningReporter() {
+  // `this` refers to the http server here
   const { address, port } = this.address();
   const protocol = this.addContext ? 'https' : 'http';
   console.log(`Listening on ${protocol}://${address}:${port}...`);
 }
 
-pmx.action('exterminate', exterminate);
-
-exterminate = async reply => {
+const exterminate = async () => {
   console.log('Exterminate, exterminate!')
   await dalek.terminate()
-  !!reply && reply({ success: true });
 }
+
+const graceful = async reply => {
+  await exterminate()
+  console.log(reply)
+  reply({ success: true });
+}
+
+pmx.action('exterminate', graceful);
 
 process.on('SIGINT', exterminate)
